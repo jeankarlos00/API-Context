bdb2c9644c2c7e7ee40ed0c12d38860466ec0cd9
Jean <jeankarlos147@gmail.com>
2024-06-27 23:45:40 -0300

    ATT

--- Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ Exo.WebApi/obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e719f7210bd8cc113ad6e3f05caf2943afe55150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
